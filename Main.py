4a0c555d46100cf448af49bcfd08c2e1d31cbb86
CrimsonWingBlackBird <sciencegeek923@gmail.com>
2021-11-30 13:37:31 -0500

    Cleaned up code

--- Main.py
+++ Main.py
@@ -3,21 +3,23 @@ import numpy as np
 from boid import Boid
 
 width = 1000
-height =1000
+height = 1000
 
 flock = [Boid(*np.random.rand(2)*1000, width, height) for _ in range(30)]
 
 
 def setup():
-    size(width,height)
+    size(width, height)
+
 
 def draw():
-    background(0,0,0)
+    background(0, 0, 0)
 
     for boid in flock:
         boid.show()
         boid.behavior(flock)
         boid.edges()
         boid.update()
+
 
 run()
--- vector2D.py
+++ vector2D.py
@@ -1,39 +1,40 @@
 import math
 
+
 class Vector2D():
 
     def __init__(self, xCoord, yCoord):
         self.x = xCoord
         self.y = yCoord
 
-    #Returns magnitude of the vector
+    # Returns magnitude of the vector
     def mag(self):
         return math.sqrt(self.x**2 + self.y**2)
 
-    #Normalizes the vector
+    # Normalizes the vector
     def normalize(self):
         return Vector2D(self.x / self.mag(), self.y / self.mag())
 
-    #Takes the dot product of two vectors
+    # Takes the dot product of two vectors
     def dot(self, other):
         return self.x * other.x + self.y * other.y
 
-    #Checks if two vectors are orthagonal
+    # Checks if two vectors are orthagonal
     def isOrthagonal(self, other):
         if self.dot(other) == 0:
             return True
         else:
             return False
 
-    #Returns the squard magnitude
+    # Returns the squard magnitude
     def magSquared(self):
         return self.x**2 + self.y**2
 
-    #Gives the component of the first vector along the second
+    # Gives the component of the first vector along the second
     def component(self, other):
         return self.dot(other) / other.mag()
 
-#Special functions
+# Special functions
 
     def __add__(self, other):
         if type(other) is Vector2D:
@@ -42,7 +43,7 @@ class Vector2D():
         else:
             x = self.x + other
             y = self.y + other
-        return Vector2D(x,y)
+        return Vector2D(x, y)
 
     def __radd__(self, other):
         return self + other
@@ -54,17 +55,17 @@ class Vector2D():
         else:
             x = self.x - other
             y = self.y - other
-        return Vector2D(x,y)
+        return Vector2D(x, y)
 
     def __rsub__(self, other):
         x = other - self.x
         y = other - self.y
-        return Vector2D(x,y)
+        return Vector2D(x, y)
 
     def __mul__(self, other):
         x = self.x * other
         y = self.y * other
-        return Vector2D(x,y)
+        return Vector2D(x, y)
 
     def __rmul__(self, other):
         return self * other
@@ -72,21 +73,20 @@ class Vector2D():
     def __truediv__(self, other):
         x = self.x / other
         y = self.y / other
-        return Vector2D(x,y)
+        return Vector2D(x, y)
 
     def __floordiv__(self, other):
         x = self.x // other
         y = self.y // other
 
     def __neg__(self):
-        return Vector2D(-self.x,-self.y)
+        return Vector2D(-self.x, -self.y)
 
     def __abs__(self):
-        return Vector2D(abs(self.x),abs(self.y))
-
+        return Vector2D(abs(self.x), abs(self.y))
 
     def __invert__(self):
-        return Vector2D(self.y,self.x)
+        return Vector2D(self.y, self.x)
 
     def __repr__(self):
         return "(" + repr(self.x) + ", " + repr(self.y) + ")"
